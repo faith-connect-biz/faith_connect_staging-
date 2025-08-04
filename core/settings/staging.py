b203d04c00ae229dd827a26a519dff820c1ba8b8
Sam <sam.mburu@pesawise.com>
2025-08-04 12:55:24 +0300

    test deployments

--- core/settings/staging.py
+++ core/settings/staging.py
@@ -1,6 +1,10 @@
 from .base import *
 import dj_database_url
 
+DEBUG = config('DEBUG', cast=bool, default=False)
+SECRET_KEY = config('SECRET_KEY')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='.onrender.com').split(',')
+
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
+    'default': dj_database_url.config(conn_max_age=600)
 }
